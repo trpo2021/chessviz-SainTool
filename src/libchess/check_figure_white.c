a3a0f8fead2de2e62e55ac1468810918b105cc4d
Vadim <vadimshemenkov@mail.ru>
2021-04-17 18:01:48 +0700

    Adding a function to define the white shape to be entered

---
+++ src/libchess/check_figure_white.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+//
+#include "check_figure_white.h"
+//
+#define Number_of_shapes 6
+//
+int check_figure_white(char s)
+{
+    char white_shapes[Number_of_shapes]
+            = {{'P'}, {'R'}, {'N'}, {'B'}, {'Q'}, {'K'}};
+    for (int i = 0; i < Number_of_shapes; i++)
+        if (s == white_shapes[i])
+            return i;
+    return 6;
+}
