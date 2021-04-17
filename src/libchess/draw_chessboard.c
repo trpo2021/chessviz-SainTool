3088622988e337be7d61e967ca2acaf95bb030a3
Vadim <vadimshemenkov@mail.ru>
2021-04-17 17:46:07 +0700

    adding a function for drawing a chess field

---
+++ src/libchess/draw_chessboard.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+//
+#include "draw_chessboard.h"
+//
+#define Size_Board 8
+#define Number_of_shapes 6
+#define Buffer 6
+//
+void draw_chessboard(char s[Size_Board][Size_Board])
+{
+    printf("  __________________");
+    for (int i = 0; i < Size_Board; i++) {
+        printf("\n");
+        printf("%d |", i + 1);
+        for (int j = 0; j < Size_Board; j++) {
+            if (s[i][j] != '-') {
+                printf("%c ", s[i][j]);
+            } else
+                printf("  ");
+        }
+        printf("|");
+    }
+    printf("\n  ------------------");
+    printf("\n   a b c d e f g h");
+}
