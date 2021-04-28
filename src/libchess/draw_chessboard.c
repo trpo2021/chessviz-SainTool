02c2c15f946dd6ee7eac061ffe1953a424df7294
SainTool <SainTool@users.noreply.github.com>
2021-04-28 11:11:46 +0700

    Changing the sequence of numbers from the side

--- src/libchess/draw_chessboard.c
+++ src/libchess/draw_chessboard.c
@@ -4,16 +4,12 @@
 //
 #include "draw_chessboard.h"
 //
-#define Size_Board 8
-#define Number_of_shapes 6
-#define Buffer 6
-//
 void draw_chessboard(char **s, const int Size_Board)
 {
     printf("  __________________");
     for (int i = 0; i < Size_Board; i++) {
         printf("\n");
-        printf("%d |", i + 1);
+        printf("%d |", Size_Board - i);
         for (int j = 0; j < Size_Board; j++) {
             if (s[i][j] != '-') {
                 printf("%c ", s[i][j]);
