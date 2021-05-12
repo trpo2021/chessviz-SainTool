ed420c1f2cd8bc440e1a57789d44535767d26820
Шеменьков Вадим Витальевич <ip015s32@bwp12.csc.local>
2021-05-12 16:41:24 +0700

    Edit code style

--- src/libchess/move_Qq.c
+++ src/libchess/move_Qq.c
@@ -1,38 +1,37 @@
 #include "move_Qq.h"
-#include "move_Rr.h"
-#include "move_Bb.h"
 #include "dont_eat_my_shape.h"
+#include "move_Bb.h"
+#include "move_Rr.h"
 
 int move_Qq(
-			char** s,
-			int pos_x_first,
-			int pos_x_second,
-			int pos_y_first,
-			int pos_y_second,
-			const int Size_Board,
-			int hod)
+        char** s,
+        int pos_x_first,
+        int pos_x_second,
+        int pos_y_first,
+        int pos_y_second,
+        const int Size_Board,
+        int hod)
 {
-	if (!(
-                    move_Bb(s,
+    if (!(
+                move_Bb(s,
+                        pos_x_first,
+                        pos_x_second,
+                        pos_y_first,
+                        pos_y_second,
+                        Size_Board,
+                        hod)))
+        if (!(
+                    move_Qq(s,
                             pos_x_first,
                             pos_x_second,
                             pos_y_first,
                             pos_y_second,
                             Size_Board,
                             hod)))
-		if (!(
-                            move_Qq(s,
-                            	     pos_x_first,
-                            	     pos_x_second,
-                            	     pos_y_first,
-                            	     pos_y_second,
-                            	     Size_Board,
-                            	     hod)))
-               	return 0;           
-    if(hod)
-    	s[pos_y_second][pos_x_second] = 'Q';
+            return 0;
+    if (hod)
+        s[pos_y_second][pos_x_second] = 'Q';
     else
-    	s[pos_y_second][pos_x_second] = 'q';
+        s[pos_y_second][pos_x_second] = 'q';
     return 1;
-    
 }
