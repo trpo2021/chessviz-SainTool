2a77b8bf8504ad3501d60d1979020e62c144832d
SainTool <Vadimshemenkov@mail.ru>
2021-05-12 13:25:07 +0700

    Add move for King

---
+++ src/libchess/move_Kk.h
@@ -0,0 +1,10 @@
+#pragma once
+
+int move_Kk(
+        char** s,
+        int pos_x_first,
+        int pos_x_second,
+        int pos_y_first,
+        int pos_y_second,
+        const int Size_Board,
+        int hod);
