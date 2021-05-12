afb7becd1ec41d9f43deaff6f9b5e8498fb1efe1
Шеменьков Вадим Витальевич <ip015s32@bwp12.csc.local>
2021-05-12 16:46:29 +0700

    Edit code style

--- src/libchess/move_Kk.c
+++ src/libchess/move_Kk.c
@@ -4,29 +4,29 @@
 #include "move_Qq.h"
 
 int move_Kk(
-			char** s,
-			int pos_x_first,
-			int pos_x_second,
-			int pos_y_first,
-			int pos_y_second,
-			const int Size_Board,
-			int hod)
+        char** s,
+        int pos_x_first,
+        int pos_x_second,
+        int pos_y_first,
+        int pos_y_second,
+        const int Size_Board,
+        int hod)
 {
-	if(!( (abs(pos_y_first - pos_y_second) == 1) && (abs(pos_x_first - pos_x_second)) == 1))
-		return 0;	
-	if (!(
-                    move_Qq(s,
-                            pos_x_first,
-                            pos_x_second,
-                            pos_y_first,
-                            pos_y_second,
-                            Size_Board,
-                            hod)))
-		return 0;           
-    if(hod)
-    	s[pos_y_second][pos_x_second] = 'K';
+    if (!((abs(pos_y_first - pos_y_second) == 1)
+          && (abs(pos_x_first - pos_x_second)) == 1))
+        return 0;
+    if (!(
+                move_Qq(s,
+                        pos_x_first,
+                        pos_x_second,
+                        pos_y_first,
+                        pos_y_second,
+                        Size_Board,
+                        hod)))
+        return 0;
+    if (hod)
+        s[pos_y_second][pos_x_second] = 'K';
     else
-    	s[pos_y_second][pos_x_second] = 'k';
+        s[pos_y_second][pos_x_second] = 'k';
     return 1;
-    
 }
